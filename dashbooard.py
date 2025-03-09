486306bdddd99e8dcf4412585f8ec0269ee814a5
StrafeAE <StrafeAE@gmail.com>
2025-03-09 12:30:36 -0400

    Update dashbooard.py

--- dashbooard.py
+++ dashbooard.py
@@ -6,46 +6,24 @@ import pandas as pd
 st.title(" :bar_chart: University Admissions Dashboard")
 df = pd.read_csv('university_student_dashboard_data.csv')
 
-col1, col2 = st.columns((2))
+col1, col2, col3 = st.columns((3))
 
 st.sidebar.header("Choose your filter: ")
 # Create for Year
 year = st.sidebar.multiselect("Pick your Year", df["Year"].unique())
-state = st.sidebar.multiselect("Pick the Term", df["Term"].unique())
+term = st.sidebar.multiselect("Pick the Term", df["Term"].unique())
+
+filtered = df.copy()
+if year:
+    filtered = df[df['Year'].isin(year)]
+if term:
+    filtered = df[df['Term'].isin(term)]
+
+with col1:
+    st.subheader('Applications')
+    st.metric('Applications', filtered['Applications'].sum())
 
 '''
-if not region:
-    df2 = df.copy()
-else:
-    df2 = df[df["Region"].isin(region)]
-  
-# Create for State
-if not state:
-    df3 = df2.copy()
-else:
-    df3 = df2[df2["State"].isin(state)]
-
-# Create for City
-
-# Filter the data based on Region, State and City
-
-if not region and not state and not city:
-    filtered_df = df
-elif not state and not city:
-    filtered_df = df[df["Region"].isin(region)]
-elif not region and not city:
-    filtered_df = df[df["State"].isin(state)]
-elif state and city:
-    filtered_df = df3[df["State"].isin(state) & df3["City"].isin(city)]
-elif region and city:
-    filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city)]
-elif region and state:
-    filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
-elif city:
-    filtered_df = df3[df3["City"].isin(city)]
-else:
-    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
-
 category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
 
 with col1:
